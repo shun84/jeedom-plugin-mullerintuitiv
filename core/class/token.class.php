ed5359fbc5eaf86de9342a73d19fd7d026df9ad2
shun84 <damien.bres@gmail.com>
2024-03-21 00:11:55 +0100

    Soucis de token avec un mot de passe faux

--- core/class/token.class.php
+++ core/class/token.class.php
@@ -29,20 +29,17 @@ class token
         if (config::byKey('access_token','mullerintuitiv') === ''){
             $this->getSession();
         }
-        log::add('mullerintuitiv','debug','Avant expires_in');
-        log::add('mullerintuitiv','debug','Time expires_in '.config::byKey('expires_in','mullerintuitiv'));
-        log::add('mullerintuitiv','debug','Time '.time());
 
         if (config::byKey('expires_in','mullerintuitiv') <= time()){
-            log::add('mullerintuitiv','debug','If expires_in');
             $refreshtoken = $mullerintuitivApi->getRefreshToken(config::byKey('refresh_token','mullerintuitiv'));
+            if ($refreshtoken->getStatusCode() !== 200){
+                log::add('mullerintuitiv','debug','If status');
+                $this->getSession();
+            }
             $refreshtokens = json_decode($refreshtoken->getBody()->getContents(), true);
             config::save('access_token',$refreshtokens['access_token'],'mullerintuitiv');
             config::save('refresh_token',$refreshtokens['refresh_token'],'mullerintuitiv');
             config::save('expires_in', time()+$refreshtokens['expires_in'],'mullerintuitiv');
-        } else {
-            log::add('mullerintuitiv','debug','Else');
-            config::remove('access_token','mullerintuitiv');
         }
 
         return config::byKey('access_token','mullerintuitiv');
